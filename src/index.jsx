729145d9b441ac6c48beafdb93b63733e71f50fd
Adam Sinning <asinning25@gmail.com>
2019-04-17 13:41:54 -0300

    react router added

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,9 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import reduxPromise from 'redux-promise';
+import { BrowserRouter as Router, Route, Redirect, Switch }
+  from 'react-router-dom';
+import { createHistory as history } from 'history';
 
 // internal modules
 import App from './components/app';
@@ -37,7 +40,12 @@ const store = createStore(reducers, initialState, middlewares);
 // render an instance of the component in the DOM
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Switch>
+        <Route path="/:channel" component={App} />
+        <Redirect from="/" to="/general" />
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
